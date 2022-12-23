1e8fed677fe0fc32aab3c6585139b6d1b7e4d5c1
David Rodriguez <davidrbacelar@gmail.com>
2022-12-23 18:33:59 +0100

    Fast committed

--- src/test/regenerate_tests.sh
+++ src/test/regenerate_tests.sh
@@ -3,6 +3,7 @@
 for f in $(ls test/lexer); do
 	echo "{}" | ./gq "test/lexer/$f/query.graphql" &>"test/lexer/$f/output.txt"
 done
+
 for f in $(ls test/parser); do
 	echo "{}" | ./gq "test/parser/$f/query.graphql" &>"test/parser/$f/output.txt"
 done
